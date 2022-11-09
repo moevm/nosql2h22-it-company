39c657ed96a82abef67a0fa57791fa15bc2388d1
Alexey Lambin <lambin1483alex@gmail.com>
2022-11-10 01:18:13 +0300

    [NIC-6] Create app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Test</h1>
+            <Button variant="contained">Test button</Button>
+        </div>
+    );
+}
+
+export default App;
