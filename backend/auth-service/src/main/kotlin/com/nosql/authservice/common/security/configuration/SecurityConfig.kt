bd170dfee38cf793579ba2827514a3c1df0ea95b
Vladimir Arutiunian <waiddew@gmail.com>
2022-11-29 04:35:19 +0300

    [NIC-19] Make the endpoint "sign up" only for the HR role

--- backend/auth-service/src/main/kotlin/com/nosql/authservice/common/security/configuration/SecurityConfig.kt
+++ backend/auth-service/src/main/kotlin/com/nosql/authservice/common/security/configuration/SecurityConfig.kt
@@ -1,24 +1,35 @@
 package com.nosql.authservice.common.security.configuration
 
+import com.nosql.authservice.enumerator.Role
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.server.WebFilter
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 class SecurityConfig {
+
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    fun securityWebFilterChain(http: ServerHttpSecurity, jwtRoleFilter: WebFilter): SecurityWebFilterChain {
         return http.csrf().disable()
-            .authorizeExchange()
-            .anyExchange().permitAll()
-            .and()
             .httpBasic().disable()
             .formLogin().disable()
+            .authorizeExchange {
+                it.pathMatchers(ADMIN_URL_PATH_PATTERN).hasAuthority(Role.HR.name)
+                    .anyExchange().permitAll()
+            }
+            .addFilterAt(jwtRoleFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             .build()
+    }
+
+    companion object {
+
+        private const val ADMIN_URL_PATH_PATTERN = "/admin/**"
     }
 }
--- backend/auth-service/src/main/kotlin/com/nosql/authservice/controller/SignUpController.kt
+++ backend/auth-service/src/main/kotlin/com/nosql/authservice/controller/SignUpController.kt
@@ -1,8 +1,10 @@
 package com.nosql.authservice.controller
 
-import com.nosql.authservice.constants.url.PUBLIC_API_V1_AUTH_URL_PATH
-import com.nosql.authservice.dto.UserDto
+import com.nosql.authservice.constants.openapi.SECURITY_SCHEME_IDENTIFIER
+import com.nosql.authservice.constants.url.ADMIN_API_V1_AUTH_URL_PATH
+import com.nosql.authservice.dto.SignUpRequestDto
 import com.nosql.authservice.service.UserService
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,22 +13,23 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-@RequestMapping(PUBLIC_API_V1_AUTH_URL_PATH)
-class UserController(
+@RequestMapping(ADMIN_API_V1_AUTH_URL_PATH)
+@SecurityRequirement(name = SECURITY_SCHEME_IDENTIFIER)
+class SignUpController(
     private val userService: UserService,
 ) {
 
     @PostMapping(
-        path = [SIGN_IN_URL_PATH],
+        path = [SIGN_UP_URL_PATH],
         consumes = [APPLICATION_JSON_VALUE],
         produces = [APPLICATION_JSON_VALUE],
     )
-    suspend fun signIn(
-        @Valid @RequestBody userDto: UserDto,
-    ) = userService.signIn(userDto)
+    suspend fun signUp(
+        @Valid @RequestBody signUpRequestDto: SignUpRequestDto,
+    ) = userService.signUp(signUpRequestDto)
 
     companion object {
 
-        const val SIGN_IN_URL_PATH = "sign-in"
+        const val SIGN_UP_URL_PATH = "sign-up"
     }
 }
