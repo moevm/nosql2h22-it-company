ca487dabb72cfc0626f53f23888ea1fcb691f660
Vladimir Arutiunian <waiddew@gmail.com>
2022-12-03 05:36:50 +0300

    Delete unused import directives

--- backend/document-service/src/main/kotlin/com/nosql/document/common/exception/MultipleHttpAuthorizationHeadersException.kt
+++ backend/document-service/src/main/kotlin/com/nosql/document/common/exception/MultipleHttpAuthorizationHeadersException.kt
@@ -1,8 +1,6 @@
 package com.nosql.document.common.exception
 
 import com.nosql.document.common.exception.enumerator.ApplicationExceptionCodeEnum.MULTIPLE_AUTHORIZATION_HEADERS
-import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.FORBIDDEN
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 
 class MultipleHttpAuthorizationHeadersException : ApplicationException(
