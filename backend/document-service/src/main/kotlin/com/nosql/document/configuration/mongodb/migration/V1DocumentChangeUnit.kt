37d87ab9808a409af70ffc84ae5ad28d416e9780
Vladimir Arutiunian <waiddew@gmail.com>
2022-12-04 22:16:06 +0300

    [NIC-11] Fix migrations in document service: add different dates for the "orderDate" field

--- backend/document-service/src/main/kotlin/com/nosql/document/configuration/mongodb/migration/V1DocumentChangeUnit.kt
+++ backend/document-service/src/main/kotlin/com/nosql/document/configuration/mongodb/migration/V1DocumentChangeUnit.kt
@@ -10,8 +10,10 @@ import io.mongock.api.annotations.ChangeUnit
 import io.mongock.api.annotations.Execution
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.MongoTemplate
+import java.time.Duration
 import java.time.Instant
 import java.util.Date
+import kotlin.random.Random
 
 @ChangeUnit(id = CHANGE_UNIT_ID, order = CHANGE_UNIT_ORDER, author = CHANGE_UNIT_AUTHOR)
 class V1DocumentChangeUnit(
@@ -29,13 +31,17 @@ class V1DocumentChangeUnit(
         val documentStatuses = DocumentStatus.values().toList()
         val userIds = listOf(HR_USER_ID, DEVELOPER_USER_ID, PROJECT_MANAGER_USER_ID)
 
+        val now = Instant.now()
+        val leftBound = now - Duration.ofDays(ONE_HUNDRED_DAYS)
+        val instants = generateInstantRange(leftBound, now)
+
         val documents = userIds.flatMap { userId ->
             documentTypes.flatMap { documentType ->
                 documentStatuses.map { documentStatus ->
                     DocumentEntity(
                         userId = ObjectId(userId),
                         type = documentType,
-                        orderDate = Date.from(Instant.now()),
+                        orderDate = Date.from(instants[Random.nextInt(MIN_INDEX, MAX_INDEX)]),
                         completeDate = resolveCompleteDateByDocumentStatus(documentStatus),
                         status = documentStatus,
                     )
@@ -44,6 +50,18 @@ class V1DocumentChangeUnit(
         }
 
         return documents
+    }
+
+    private fun generateInstantRange(from: Instant, to: Instant): List<Instant> {
+        var currentInstant = from
+        val instants = mutableListOf<Instant>()
+
+        while (currentInstant < to) {
+            currentInstant += Duration.ofDays(ONE_DAY)
+            instants.add(currentInstant)
+        }
+
+        return instants
     }
 
     private fun resolveCompleteDateByDocumentStatus(documentStatus: DocumentStatus) =
@@ -61,5 +79,10 @@ class V1DocumentChangeUnit(
         private const val HR_USER_ID = "638b9e21fe9cfa34d93be93d"
         private const val DEVELOPER_USER_ID = "638b9e21fe9cfa34d93be93b"
         private const val PROJECT_MANAGER_USER_ID = "638b9e21fe9cfa34d93be93f"
+
+        private const val ONE_DAY = 1L
+        private const val ONE_HUNDRED_DAYS = 100L
+        private const val MIN_INDEX = 0
+        private const val MAX_INDEX = 100
     }
 }
